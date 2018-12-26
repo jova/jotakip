e09ece94ae7e860c06885b6701265135c83e76da
Ersoy TORAMAN <jovannijares@gmail.com>
2018-12-26 22:09:05 +0300

    ILoginService eklendi.

---
+++ JoTakip - Bilgisayar Stok Takip Sistemi/Business/Concrete/LoginManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class LoginManager : ILoginService
+    {
+        private IUserDal userDal;
+
+        public LoginManager(IUserDal userDal)
+        {
+            this.userDal = userDal;
+        }
+
+        public bool Login(string username, string password)
+        {
+            User user = userDal.Get(x => x.Username == username);
+
+            if (user == null) return false;
+            return user.Password == password;
+        }
+    }
+}
